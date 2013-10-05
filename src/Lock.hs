f124cd6ef13e5116a408403aa9e0c801e9f0424c
greg <code@gregorkopf.de>
2013-10-05 19:33:27 +0200

    FreeBSD support

--- src/Lock.hs
+++ src/Lock.hs
@@ -7,13 +7,13 @@ import qualified Control.Exception as E
 
 -- | Try to acquire the lock, return `True` on success
 acquire :: IO Bool
-acquire = (semOpen "pwsafe" (OpenSemFlags True True) readWriteMode 0 >> return True) `E.catch` handlerFalse
+acquire = (semOpen "/pwsafe" (OpenSemFlags True True) readWriteMode 0 >> return True) `E.catch` handlerFalse
   where
     readWriteMode = ownerReadMode `unionFileModes` ownerWriteMode
 
 -- | Release the lock, return `True` on success
 release :: IO Bool
-release = (semUnlink "pwsafe" >> return True) `E.catch` handlerFalse
+release = (semUnlink "/pwsafe" >> return True) `E.catch` handlerFalse
 
 -- | Return `False` on `E.SomeException`
 handlerFalse :: E.SomeException -> IO Bool
